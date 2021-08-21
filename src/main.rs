6b9d19ba4bfc4c9125d85ac102013f39eb62756c
linkviii <LinkEighth@gmail.com>
2021-08-21 16:36:38 -0400

    Determine if the contents of a directory are tracked

--- src/main.rs
+++ src/main.rs
@@ -28,7 +28,7 @@ fn is_tracked(p: &Path, index: &git2::Index) -> bool {
 }
 
 fn strip_dot(p: &Path) -> PathBuf {
-    p.into_iter().filter(|&x| x!= ".").collect()
+    p.into_iter().filter(|&x| x != ".").collect()
 }
 
 fn main() {
@@ -60,6 +60,61 @@ fn main() {
     // println!("world world world");
 }
 
+#[derive(Debug)]
+enum Trackedness {
+    All,
+    Some,
+    None,
+}
+
+// Recursivly check all children of p
+fn has_tracked(p: &Path, index: &git2::Index) -> Trackedness {
+    if p.is_file() {
+        match is_tracked(p, &index) {
+            true => return Trackedness::All,
+            false => return Trackedness::None,
+        }
+    }
+
+    let mut any_tracked = false;
+    let mut any_untracked = false;
+
+    let mut dir = p.read_dir().unwrap().peekable();
+
+    if dir.peek().is_none() {
+        // Empty dir cannot be tracked
+        return Trackedness::None;
+    }
+
+    for entry in dir {
+        let epathbuf = entry.unwrap().path();
+        let epath = epathbuf.as_path();
+
+        match has_tracked(epath, &index) {
+            Trackedness::All => any_tracked = true,
+            Trackedness::Some => {
+                any_tracked = true;
+                any_untracked = true;
+            }
+            Trackedness::None => any_untracked = true,
+        };
+
+        if any_tracked && any_untracked {
+            break;
+        }
+    }
+
+    if any_tracked {
+        if any_untracked {
+            return Trackedness::Some;
+        } else {
+            return Trackedness::All;
+        }
+    } else {
+        return Trackedness::None;
+    }
+}
+
 fn foobar(index: &git2::Index) {
     use lscolors::{LsColors, Style};
     let lscolors = LsColors::from_env().unwrap_or_default();
@@ -79,6 +134,8 @@ fn foobar(index: &git2::Index) {
         let color_p = format!("{}", style.paint(strip_dot(p).to_str().unwrap()));
         if p.is_dir() {
             println!("d {}", color_p);
+            let track = has_tracked(p, &index);
+            println!("{:?}", track);
         } else if is_tracked(p, &index) {
             println!("t {}", color_p);
         } else {
